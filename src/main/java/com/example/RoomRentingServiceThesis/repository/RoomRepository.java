78b57be408a17fbbb1698d9d20c52f96c9ccdddc
Vichea leng <vichealeng45@gmail.com>
2026-03-27 22:55:19 +0700

    Update RoomRepository: add custom query methods

--- src/main/java/com/example/RoomRentingServiceThesis/repository/RoomRepository.java
+++ src/main/java/com/example/RoomRentingServiceThesis/repository/RoomRepository.java
@@ -5,8 +5,9 @@ import com.example.RoomRentingServiceThesis.entity.Room;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface RoomRepository extends JpaRepository<Room, Long>, JpaSpecificationExecutor<Room> {
 
     Page<Room> findByLocationContainingIgnoreCase(String location, Pageable pageable);
 
