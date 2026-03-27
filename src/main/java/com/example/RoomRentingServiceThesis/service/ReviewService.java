af776a9bcd3c7a6bdbfc3db0680803f9d5f3a17d
Vichea leng <vichealeng45@gmail.com>
2026-03-27 22:55:21 +0700

    Update ReviewService: add getReviewsByRoom method

--- src/main/java/com/example/RoomRentingServiceThesis/service/ReviewService.java
+++ src/main/java/com/example/RoomRentingServiceThesis/service/ReviewService.java
@@ -1,12 +1,14 @@
 package com.example.RoomRentingServiceThesis.service;
 
+import com.example.RoomRentingServiceThesis.entity.Review;
+
 import java.util.List;
 
 public interface ReviewService {
 
     Long createReview(Long roomId, Long renterId, int rating, String comment);
 
-    List<Object> getReviewsByRoom(Long roomId);
+    List<Review> getReviewsByRoom(Long roomId);
 
     void deleteReview(Long reviewId);
 }
