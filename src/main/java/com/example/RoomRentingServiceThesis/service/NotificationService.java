7c34647c525e7b4d356a39fffbc783403c573d45
Vichea leng <vichealeng45@gmail.com>
2026-03-28 23:49:35 +0700

    Update NotificationService interface (if changed)

--- src/main/java/com/example/RoomRentingServiceThesis/service/NotificationService.java
+++ src/main/java/com/example/RoomRentingServiceThesis/service/NotificationService.java
@@ -1,10 +1,10 @@
-package com.example.RoomRentingServiceThesis.service;
-
-import java.util.List;
-
-public interface NotificationService {
-
-    List<Object> getUserNotifications(Long userId, String userType);
-
-    void markAsRead(Long notificationId);
-}
+//package com.example.RoomRentingServiceThesis.service;
+//
+//import java.util.List;
+//
+//public interface NotificationService {
+//
+//    List<Object> getUserNotifications(Long userId, String userType);
+//
+//    void markAsRead(Long notificationId);
+//}
