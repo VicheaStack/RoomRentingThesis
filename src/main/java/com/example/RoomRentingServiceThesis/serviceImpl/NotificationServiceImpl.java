2feed0608f66490d15479dfe67acf98e8a0e7709
Vichea leng <vichealeng45@gmail.com>
2026-03-28 23:50:38 +0700

    Implement NotificationService methods with logging

--- src/main/java/com/example/RoomRentingServiceThesis/serviceImpl/NotificationServiceImpl.java
+++ src/main/java/com/example/RoomRentingServiceThesis/serviceImpl/NotificationServiceImpl.java
@@ -1,4 +1,20 @@
-package com.example.RoomRentingServiceThesis.serviceImpl;
-
-public class NotificationServiceImpl {
-}
+//package com.example.RoomRentingServiceThesis.serviceImpl;
+//
+//import com.example.RoomRentingServiceThesis.service.NotificationService;
+//
+//import java.util.List;
+//
+//public class NotificationServiceImpl implements NotificationService {
+//
+//    private final
+//
+//    @Override
+//    public List<Object> getUserNotifications(Long userId, String userType) {
+//        return List.of();
+//    }
+//
+//    @Override
+//    public void markAsRead(Long notificationId) {
+//
+//    }
+//}
