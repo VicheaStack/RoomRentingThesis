5c0b051b394ed61d1e76e15fe202b9ec3aa5d3ab
Vichea leng <vichealeng45@gmail.com>
2026-03-28 23:49:11 +0700

    Define addFavorite, removeFavorite, getFavoritesByRenter

--- src/main/java/com/example/RoomRentingServiceThesis/service/FavoriteService.java
+++ src/main/java/com/example/RoomRentingServiceThesis/service/FavoriteService.java
@@ -1,12 +1,14 @@
 package com.example.RoomRentingServiceThesis.service;
 
+import com.example.RoomRentingServiceThesis.entity.Favorite;
+
 import java.util.List;
 
 public interface FavoriteService {
 
-    void addFavorite(Long renterId, Long roomId);
+    Favorite addFavorite(Long renterId, Long roomId);
 
-    void removeFavorite(Long renterId, Long roomId);
+    Favorite removeFavorite(Long renterId, Long roomId);
 
-    List<Object> getFavoritesByRenter(Long renterId);
+    List<Favorite> getFavoritesByRenter(Long renterId);
 }
